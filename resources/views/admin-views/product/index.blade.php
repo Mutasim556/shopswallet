ab60df6caad6994bd109d484b952695bd952f77a
Md Mutasim Naib <mutasim@inceptapharma.com>
2024-03-04 17:01:16 +0600

    test

--- resources/views/admin-views/product/index.blade.php
+++ resources/views/admin-views/product/index.blade.php
@@ -17,7 +17,7 @@
             </span>
             <span>
                 @if(Config::get('module.current_module_type') == 'services')
-                    {{ translate('messages.add_new_service') }} ssss
+                    {{ translate('messages.add_new_service') }}
                 @else
                     {{ translate('messages.add_new_item') }}
                 @endif
